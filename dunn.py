1bf9dc9c0c39d5456c8ff830a3b311e073332da5
albanigiulia <g.albani6@campus.unimib.it>
2024-10-15 16:40:52 +0200

    applicazione k means ai dati di matrici_zero_uno

--- dunn.py
+++ dunn.py
@@ -1,17 +1,20 @@
 import random
 from random import randint
 # per intallare il pacchetto clusteval esegui "pip intstall clusteval"
-from clusteval import clusteval
+#from clusteval import clusteval
 from sklearn.cluster import KMeans
 #from random import randint
 import numpy as np
 
+#creo una matrice di 0 e 1 ordinati
+matrix_zero_uno_or = [[0,0,0,0], [0,0,0,0], [1,1,1,1], [1,1,1,1]] 
+
 #creo una matrice di 0 e 1
 matrix_zero_uno = []
 
-for i in range(10):
+for i in range(4):
     n = []
-    for j in range(10):
+    for j in range(4):
         number = randint(0,1)
         n.insert(i,number)
     matrix_zero_uno.append(n)
@@ -20,9 +23,9 @@ print(matrix_zero_uno)
 #creo una matrice di numeri compresi fra 0 e 1
 matrix_compresi = []
 
-for i in range(10):
+for i in range(4):
     n = []
-    for j in range(10):
+    for j in range(4):
         number = random.uniform(0, 1)
         n.insert(i,number)
     matrix_compresi.append(n)
@@ -31,21 +34,28 @@ print(matrix_compresi)
 #creo una matrice di numeri compresi fra 0 e 9
 matrix3 = []
 
-for i in range(10):
+for i in range(4):
     n = []
-    for j in range(10):
+    for j in range(4):
         number = random.uniform(0, 9)
         n.insert(i,number)
     matrix_compresi.append(n)
 print(matrix_compresi)
 
-# Applicare KMeans con k=2 ai dati della matrice
+# Applicare KMeans con k=2 ai dati della matrice matrix_zero_uno_or
 kmeans = KMeans(n_clusters=2)
-labels = kmeans.fit_predict(matrix_zero_uno)
-# labels conterrà il cluster assegnato a ciascun punto
-print(labels)
+kmeans.fit(matrix_zero_uno_or)
+print("Centri dei cluster:", kmeans.cluster_centers_)
+print("Etichette assegnate:", kmeans.labels_) 
+
+# Applicare KMeans con k=2 ai dati della matrice matrix_zero_uno
+kmeans1 = KMeans(n_clusters=2)
+kmeans1.fit(matrix_zero_uno)
+print("Centri dei cluster:", kmeans1.cluster_centers_)
+print("Etichette assegnate:", kmeans1.labels_) #Ogni riga della matrice M è assegnata a uno dei due cluster (0 o 1).
+
 
 # Inizializzare Clusteval con la metrica Dunn
-ce = clusteval(metric='dunn')
+#ce = clusteval(metric='dunn')
 #dunn_index1 = ce.fit(matrix_zero_uno)
 #print(dunn_index1)
